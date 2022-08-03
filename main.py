2904063c0d49c406d2e19294af59c0a663d1df6a
Billy Lam <0chun0@gmail.com>
2022-08-03 18:07:38 +0800

    add dummy datafrmae

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import pandas as pd
+
+
 def main():
     print("Main")
 
 
 def dev():
     print("dev")
+    s = pd.Series(list('abca'))
+    df = pd.get_dummies(s)
+    print(df.head())
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    dev()
